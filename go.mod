5aa9e95ecda32780007f9fbcd65092b68e0b03c9
mohemohe <mohemohe@ghippos.net>
2024-07-16 03:37:48 +0900

    support v2

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/lxbot/store-file
 
 go 1.13
+
+require (
+	github.com/lxbot/lxlib/v2 v2.0.0
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+)
--- store.go
+++ store.go
@@ -2,38 +2,57 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"os"
+	"sync"
+
+	"github.com/lxbot/lxlib/v2"
+	"github.com/lxbot/lxlib/v2/common"
+	"github.com/lxbot/lxlib/v2/lxtypes"
 )
 
 type M = map[string]interface{}
 
 const fileName = "store.json"
-var ch *chan M
 
-func Boot(c *chan M) {
-	ch = c
+var wg sync.WaitGroup
+
+func main() {
+	store, getCh, setCh := lxlib.NewStore()
+
+	for {
+		select {
+		case event := <-*getCh:
+			onGet(event, store)
+		case event := <-*setCh:
+			onSet(event)
+		}
+	}
 }
 
-func Set(key string, value interface{}) {
-	m := map[string]interface{}{}
-	if b, err := ioutil.ReadFile(fileName); err == nil {
+func onGet(event *lxtypes.StoreEvent, store *lxlib.Store) {
+	m := M{}
+	if b, err := os.ReadFile(fileName); err == nil {
 		_ = json.Unmarshal(b, &m)
 	}
 
-	m[key] = value
+	if value, ok := m[event.Key]; ok {
+		event.Value = value
+	}
+
+	store.SendGetResult(event)
+}
+
+func onSet(event *lxtypes.StoreEvent) {
+	m := M{}
+	if b, err := os.ReadFile(fileName); err == nil {
+		_ = json.Unmarshal(b, &m)
+	}
+
+	m[event.Key] = event.Value
 
 	b, err := json.Marshal(m)
 	if err != nil {
-		panic(err)
+		common.DebugLog(err)
 	}
-	_ = ioutil.WriteFile(fileName, b, 0600)
-}
-
-func Get(key string) interface{} {
-	m := map[string]interface{}{}
-	if b, err := ioutil.ReadFile(fileName); err == nil {
-		_ = json.Unmarshal(b, &m)
-	}
-
-	return m[key]
+	_ = os.WriteFile(fileName, b, 0600)
 }
